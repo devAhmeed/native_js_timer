f6aafee65525d96f6ba8cd24df4bc69f82b862c4
Ahmed Hesham Kamal <77901768+AhmedHesham01@users.noreply.github.com>
2021-07-27 01:36:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// Elements 
+const daysEl = document.querySelector(".days");
+const hourEl = document.querySelector(".hours");
+const minsEl = document.querySelector(".mins");
+const secondsEl = document.querySelector(".seconds");
+/////////////////////////////////////////////
+
+
+
+
+
+
+setInterval(function(){
+  const now = new Date();
+  const date = new Date(2022 , 5 , 11 , 10);
+  const rem = date - now;
+  const oneDay= 24*60*60*1000;
+  const oneHour = 60*60*1000;
+  const oneMin = 60*1000;
+  const secondsLeft = Math.trunc(rem / 1000);
+  const minsLeft = Math.trunc(rem / oneMin);
+  const hoursLeft = Math.trunc(rem / oneHour);
+  const daysLeft = Math.trunc(rem / oneDay);
+  daysEl.textContent = `${daysLeft} Days`;
+  hourEl.textContent = `${hoursLeft} Hours`;
+  minsEl.textContent = `${minsLeft} Minutes`;
+  secondsEl.textContent = `${secondsLeft} Seconds`;
+},1000)
